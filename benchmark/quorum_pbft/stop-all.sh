faa5b18a8fd03949914b9c90ab476d558fa08cce
Your Name <you@example.com>
2019-01-25 15:17:59 +0800

    quorum install

--- benchmark/quorum_pbft/stop-all.sh
+++ benchmark/quorum_pbft/stop-all.sh
@@ -5,13 +5,13 @@ cd `dirname ${BASH_SOURCE-$0}`
 
 i=0
 for host in `cat $CLIENTS`; do
-    ssh -oStrictHostKeyChecking=no fuwanzeng@$host killall -KILL driver 
+    ssh -oStrictHostKeyChecking=no $host killall -KILL driver 
     echo done node $host
 done
 
 for host in `cat $HOSTS`; do
   if [[ $i -lt $1 ]]; then
-    ssh -oStrictHostKeyChecking=no fuwanzeng@$host $QUO_HOME/stop.sh
+    ssh -oStrictHostKeyChecking=no $host $QUO_HOME/stop.sh
     echo done node $host
   fi
   let i=$i+1
