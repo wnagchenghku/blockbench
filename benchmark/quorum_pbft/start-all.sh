ce67eab4c48a7fa14072e3162712dbd957d11630
Your Name <you@example.com>
2019-01-26 13:28:53 +0800

    nit

--- benchmark/quorum_pbft/start-all.sh
+++ benchmark/quorum_pbft/start-all.sh
@@ -3,10 +3,10 @@
 cd `dirname ${BASH_SOURCE-$0}`
 . env.sh
 
-# echo "start-all.sh"
-# rm -rf addPeer.txt
-# ./gather.sh $1 
-# sleep 3
+echo "start-all.sh"
+rm -rf addPeer.txt
+./gather.sh $1 
+sleep 3
 
 i=0
 for host in `cat $HOSTS`; do
@@ -17,11 +17,11 @@ for host in `cat $HOSTS`; do
   let i=$i+1
 done
 
-# i=0
-# for host in `cat $HOSTS`; do
-#   if [[ $i -lt $1 ]]; then
-#     ssh -oStrictHostKeyChecking=no  $host $QUO_HOME/start-addPeer.sh $i
-#     echo done node $host
-#   fi
-#   let i=$i+1
-# done
+i=0
+for host in `cat $HOSTS`; do
+  if [[ $i -lt $1 ]]; then
+    ssh -oStrictHostKeyChecking=no  $host $QUO_HOME/start-addPeer.sh $i
+    echo done node $host
+  fi
+  let i=$i+1
+done
